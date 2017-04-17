d1e20e13927b5847412bda782f5d2d12aa10a3fe
Justus <juha-pekka.arimaa@ctrlentertainment.fi>
2017-04-17 13:50:08 +0300

    Added VertexPulling-project

---
+++ VertexPulling/Source/VertexPullingApplication.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Framework/Application.h>
+#include <Framework/Camera.h>
+#include <Framework/CameraController.h>
+#include <Framework/Shader.h>
+#include <Framework/Model.h>
+#include <Framework/Image.h>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+class VertexPullingApplication : public fw::Application
+{
+public:
+	explicit VertexPullingApplication();
+	virtual ~VertexPullingApplication();
+	VertexPullingApplication(const VertexPullingApplication&) = delete;
+	VertexPullingApplication(VertexPullingApplication&&) = delete;
+	VertexPullingApplication& operator=(const VertexPullingApplication&) = delete;
+	VertexPullingApplication& operator=(VertexPullingApplication&&) = delete;
+
+	virtual bool initialize() final;
+	virtual void update() final;
+	virtual void render() final;
+	virtual void gui() final;
+
+private:
+	fw::Camera camera;
+	fw::CameraController cameraController;
+	fw::Shader shader;
+	fw::Image image;
+	fw::Transformation objTransformation;
+	glm::mat4 mvpMatrix;	
+	GLuint VAO = 0;
+	GLuint vertexBuffer = 0;
+	GLuint indexBuffer = 0;
+	unsigned int numIndices = 0;
+
+	void createBuffers(const fw::Model& model);
+};
