5c23298eb6f89e4eff1feda9fe1278b69f2407bc
JP Arimaa <juhapekka.arimaa@gmail.com>
2019-11-13 21:35:07 +0200

    Initial works for shadowmap rendering

--- Experiments/VolumetricFog/src/FogApplication.h
+++ Experiments/VolumetricFog/src/FogApplication.h
@@ -21,7 +21,7 @@ public:
     virtual bool initialize() final;
     virtual void update() final;
     virtual void render() final;
-    virtual void gui() final;
+    virtual void gui() final{};
 
 private:
     struct RenderObject
@@ -33,15 +33,27 @@ private:
     fw::Camera camera;
     fw::CameraController cameraController;
     fw::Shader diffuseShader;
+    fw::Shader shadowMapShader;
+    fw::Shader simpleShader;
     fw::Image image;
+
     std::vector<RenderObject> renderObjects;
+    std::vector<RenderObject> lightRenderObjects;
+
+    std::vector<glm::vec3> lightPositions;
     std::vector<glm::vec3> lightDirections;
     std::vector<glm::vec4> lightColors;
+    std::vector<glm::mat4> lightSpaceMatrices;
+
     GLuint VAO = 0;
     GLuint vertexBuffer = 0;
     GLuint indexBuffer = 0;
     unsigned int numIndices = 0;
 
+    std::vector<GLuint> shadowMapBuffers;
+    std::vector<GLuint> shadowMapTextures;
+
     void createBuffers(const fw::Model& model);
+    void createShadowMaps();
     void createScene();
 };
